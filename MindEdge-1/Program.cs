72029cb2da3a861856951d709806e27f5fed0701
Mariam-065 <mariemmohammed065@gmail.com>
2026-02-27 13:54:51 +0200

    Enable Swagger for production

--- MindEdge-1/Program.cs
+++ MindEdge-1/Program.cs
@@ -39,12 +39,8 @@ builder.Services.AddAuthentication(options => {
 
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
